0f37f7161ecf6e538fe236f8a7749c0f66f268fe
pavansarvaiya58 <pavansarvaiya58@gmail.com>
2023-09-23 16:13:06 -0600

    initial code

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emoji
+//
+//  Created by Pavan Sarvaiya on 23/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
