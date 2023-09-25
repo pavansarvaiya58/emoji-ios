3419553f0f5dc25cd0656d1d474e249d7d3e5ce8
pavansarvaiya58 <pavansarvaiya58@gmail.com>
2023-09-24 23:30:33 -0600

    final emoji selector

--- ContentView.swift
+++ ContentView.swift
@@ -15,15 +15,20 @@ struct ContentView: View {
     @State var selection: Emoji = .third
     
     var body: some View {
-        VStack {
-            Text(selection.rawValue)
-                .font(.system(size: 120))
-            
-            Picker("Select Emoji", selection: $selection) {
-                ForEach(Emoji.allCases, id: \.self) { emoji in
-                    Text(emoji.rawValue)
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 120))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
                 }
+                .pickerStyle(.segmented)
             }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
         }
     }
 }
